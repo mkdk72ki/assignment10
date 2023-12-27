9a5725cb8918b07a489f2a07abd3c2e4816af996
mkdk72ki <harukazexsysy@gmail.com>
2023-12-27 23:10:40 +0900

    不要な箇所を削除

--- src/main/java/com/mkdk72ki/asgmt10/exception/UserNotFoundException.java
+++ src/main/java/com/mkdk72ki/asgmt10/exception/UserNotFoundException.java
@@ -2,22 +2,8 @@ package com.mkdk72ki.asgmt10.exception;
 
 public class UserNotFoundException extends RuntimeException {
 
-  public UserNotFoundException() {
-    super();
-  }
-
-  // 指定されたメッセージと原因を持つ例外を作成する
-  public UserNotFoundException(String message, Throwable cause) {
-    super(message, cause);
-  }
-
   // 指定されたメッセージを持つ例外を作成する
   public UserNotFoundException(String message) {
     super(message);
-  }
-
-  // 指定された原因を持つ例外を作成する
-  public UserNotFoundException(Throwable cause) {
-    super(cause);
   }
 }
